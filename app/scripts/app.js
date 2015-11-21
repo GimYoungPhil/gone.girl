f5404d9e87c4e593655ae923592c79a5af27115e
GimYoungPhil <youngphil.gim@gmail.com>
2015-11-21 16:43:08 +0900

    app style

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,6 +16,27 @@
   });
 
 
+  addEventListener('paper-header-transform', function(e) {
+    var appName = document.querySelector('#mainToolbar .app-name');
+    var middleContainer = document.querySelector('#mainToolbar .middle-container');
+    var bottomContainer = document.querySelector('#mainToolbar .bottom-container');
+    var detail = e.detail;
+    var heightDiff = detail.height - detail.condensedHeight;
+    var yRatio = Math.min(1, detail.y / heightDiff);
+    var maxMiddleScale = 0.50;  // appName max size when condensed. The smaller the number the smaller the condensed size.
+    var scaleMiddle = Math.max(maxMiddleScale, (heightDiff - detail.y) / (heightDiff / (1-maxMiddleScale))  + maxMiddleScale);
+    var scaleBottom = 1 - yRatio;
+
+    // Move/translate middleContainer
+    Polymer.Base.transform('translate3d(0,' + yRatio * 100 + '%,0)', middleContainer);
+
+    // Scale bottomContainer and bottom sub title to nothing and back
+    Polymer.Base.transform('scale(' + scaleBottom + ') translateZ(0)', bottomContainer);
+
+    // Scale middleContainer appName
+    Polymer.Base.transform('scale(' + scaleMiddle + ') translateZ(0)', appName);
+  });
+
   app.onDataRouteClick = function() {
     var drawerPanel = document.querySelector('#paperDrawerPanel');
     if (drawerPanel.narrow) {
@@ -23,18 +44,12 @@
     }
   };
 
-
   app.scrollPageToTop = function() {
     document.getElementById('mainContainer').scrollTop = 0;
   };
 
   app._equal = function(value1, value2) {
     return value1 === value2;
-  };
-
-  app.handleResponse = function(event, element) {
-    console.log(event.detail.response);
-
   };
 
 })(document);
